4e71dac5923456b292b720ad552e53c36b76ec83
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2021-08-31 14:08:18 +0200

    Add README.md and .gitignore

---
+++ Unity-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unity-iOS
+//
+//  Created by Mahmoud Aziz on 31/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
