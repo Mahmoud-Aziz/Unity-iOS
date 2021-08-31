ec531f25b4d17ba1bf7e7f91587e1bf85ac41d55
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2021-08-31 14:16:40 +0200

    Add simple button to the home screen to test the integration

--- Unity-iOS/ViewController.swift
+++ Unity-iOS/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UnityFrameworkDeallocator {
+    
+    var unityView: UIView?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func show(_ sender: Any) {
+        UnityFrameworkWrapper.shared.delegate = self
+        self.unityView = UnityFrameworkWrapper.shared.framework.appController()!.rootView
+        self.view.addSubview(self.unityView!)
     }
-
-
+    
+    // MARK: - UnityFrameworkDeallocator
+    
+    func exitUnity() {
+        unityView?.removeFromSuperview()
+    }
+    
 }
 
